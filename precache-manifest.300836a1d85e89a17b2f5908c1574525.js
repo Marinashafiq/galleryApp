39f4923bc619ba844366401fcd220f7443e7fa1c
Marina Magdy <Marina.Magdy@ibtikar.com>
2019-12-31 13:47:17 +0200

    Updates

---
+++ precache-manifest.300836a1d85e89a17b2f5908c1574525.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7661a00f3372077b67a9c268535306c9",
+    "url": "/gallery-app/index.html"
+  },
+  {
+    "revision": "5322f40cab06129c1e44",
+    "url": "/gallery-app/static/css/3.6e6feccd.chunk.css"
+  },
+  {
+    "revision": "d94f586ec727b0b9c4b8",
+    "url": "/gallery-app/static/css/5.a3cf3f70.chunk.css"
+  },
+  {
+    "revision": "b8f1beea3dd9ebc33cde",
+    "url": "/gallery-app/static/css/6.d18bcd11.chunk.css"
+  },
+  {
+    "revision": "e60cdb857e211321e186",
+    "url": "/gallery-app/static/css/7.b5c72deb.chunk.css"
+  },
+  {
+    "revision": "02205317863b5a7af9d5",
+    "url": "/gallery-app/static/css/8.0dabc02a.chunk.css"
+  },
+  {
+    "revision": "edf306061753fcf4d50b",
+    "url": "/gallery-app/static/css/main.1257b9da.chunk.css"
+  },
+  {
+    "revision": "8b04d751cd926510f618",
+    "url": "/gallery-app/static/js/0.689f2a9c.chunk.js"
+  },
+  {
+    "revision": "5322f40cab06129c1e44",
+    "url": "/gallery-app/static/js/3.add113f5.chunk.js"
+  },
+  {
+    "revision": "9f966e0718216656d4e5",
+    "url": "/gallery-app/static/js/4.28f8a592.chunk.js"
+  },
+  {
+    "revision": "d94f586ec727b0b9c4b8",
+    "url": "/gallery-app/static/js/5.5c65308e.chunk.js"
+  },
+  {
+    "revision": "b8f1beea3dd9ebc33cde",
+    "url": "/gallery-app/static/js/6.0f7cfb03.chunk.js"
+  },
+  {
+    "revision": "e60cdb857e211321e186",
+    "url": "/gallery-app/static/js/7.5f738fac.chunk.js"
+  },
+  {
+    "revision": "02205317863b5a7af9d5",
+    "url": "/gallery-app/static/js/8.ecd84d20.chunk.js"
+  },
+  {
+    "revision": "edf306061753fcf4d50b",
+    "url": "/gallery-app/static/js/main.b5e9152d.chunk.js"
+  },
+  {
+    "revision": "a404b2344fffea467758",
+    "url": "/gallery-app/static/js/runtime-main.98062fc8.js"
+  },
+  {
+    "revision": "e05090b757da107c30910405cdf6154a",
+    "url": "/gallery-app/static/media/bodyBg.e05090b7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gallery-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gallery-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gallery-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gallery-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gallery-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
