9f411d580249d3668b95ed31f11c3a21d5412dbc
Marina Magdy <Marina.Magdy@ibtikar.com>
2019-12-31 14:49:55 +0200

    change route

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import { Routes } from './routes/index';
 function App() {
   return (
        <Routes/> 
-    // <div className="App">
-    //     {/* <Router>{Routes}</Router> */}
-    // {/* <h1>hjgh</h1> */}
-
-    // </div>
   );
 }
 
